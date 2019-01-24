1d5bbc58e350595e8903e8f5896e5df470fc3961
naturalbornchiller <mail@emmanuelprice.com>
2019-01-24 13:43:09 -0500

    Moves virtuals from Chain to Task model so that the data can be used easily on the front end.

--- app/models/chain.js
+++ app/models/chain.js
@@ -24,27 +24,12 @@ const chainSchema = new mongoose.Schema(
   }
 )
 
-// get if chain can be concated
-chainSchema.virtual('concatAvailable').get(function () {
-  return !this.dateBroken && (new Date() - this.lastConcat > 86400000)
-})
-
 // gets the length of the chain (in days)
 chainSchema.virtual('length').get(function () {
   // time difference in ms between dateChainWasStarted
   // and dateOfLastConcat, divided by number of ms in a day,
   // and floored for convenience
   return Math.floor((this.lastConcat - this.dateStarted) / 86400000)
-})
-
-// gets the countdown until the chain breaks (in hrs)
-chainSchema.virtual('hoursToBreak').get(function () {
-  const { lastConcat, dateBroken } = this
-  if (!dateBroken) {
-    return Math.ceil(48 - ((new Date() / 86400000) - (new Date(lastConcat) / 86400000)) * 24)
-  } else {
-    return false
-  }
 })
 
 const Chain = mongoose.model('Chain', chainSchema)
--- app/models/task.js
+++ app/models/task.js
@@ -19,7 +19,7 @@ const taskSchema = new mongoose.Schema(
   }
 )
 
-// returns index of longest chain for this task
+// get index of longest chain for this task
 taskSchema.virtual('longestChainIndex').get(function () {
   // stores the length of the biggest chain in the array
   const maxLength = Math.max.apply(null, this.chains.map(c => c.length))
@@ -30,14 +30,53 @@ taskSchema.virtual('longestChainIndex').get(function () {
   return this.chains.findIndex(c => c.length === maxLength)
 })
 
-// returns all days inwhich task was completed
+// get all days inwhich task was completed
 taskSchema.virtual('combinedChainLength').get(function () {
   return this.chains.reduce((total, chain) => total + chain.length, 0)
 })
 
-// returns date that task was first started
+// get date that task was first started
 taskSchema.virtual('taskStarted').get(function () {
   return this.chains.length ? this.chains[0].dateStarted : false
+})
+
+// get whether or not a new chain can be made
+taskSchema.virtual('createChainAvailable').get(function () {
+  // index of the last chain
+  const latestChainIdx = this.chains.length - 1
+
+  // if the task has no chains or if the last chain is broken, add a chain
+  return this.chains.length === 0 || this.chains[latestChainIdx].dateBroken
+})
+
+// get if last chain can be concated
+taskSchema.virtual('concatAvailable').get(function () {
+  // if the task has chains the concat is available
+  if (this.chains.length > 0) {
+    // index of the last chain
+    const latestChainIdx = this.chains.length - 1
+
+    // whether the latest chain is NOT broken AND it's been over 24hrs since the last concat
+    return !this.chains[latestChainIdx].dateBroken && (new Date() - this.chains[latestChainIdx].lastConcat > 86400000)
+  } else {
+    // otherwise concat is NOT available
+    return false
+  }
+})
+
+// gets the countdown until the latest chain breaks (in hrs)
+chainSchema.virtual('hoursToBreak').get(function () {
+  // index of the last chain
+  const latestChainIdx = this.chains.length - 1
+
+  // if the task has chains AND the latest chain is not broken
+  if (this.chains.length > 0 && !this.chains[latestChainIdx].dateBroken) {
+    // return the time until it breaks
+    return Math.ceil(48 - ((new Date() / 86400000) - (new Date(lastConcat) / 86400000)) * 24)
+  } else {
+    // otherwise send false
+    return false
+  }
 })
 
 const Task = mongoose.model('Task', taskSchema)
