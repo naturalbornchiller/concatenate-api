21c2e1d984ca7d0157fe2e98b613d83543081555
Zen <zen@Emmanuels-MacBook-Air.local>
2019-02-25 19:00:35 -0500

    fixes error in chain.length calculation

--- app/models/chain.js
+++ app/models/chain.js
@@ -30,11 +30,11 @@ chainSchema.virtual('length').get(function () {
   // today and last concat floored
   const chainStart = moment(this.dateStarted)
     .hours(0).minutes(0).seconds(0).milliseconds(0)
-  const chainEnd = moment(this.lastConcat)
+  const chainEnd = moment(this.dateBroken || this.lastConcat)
     .hours(0).minutes(0).seconds(0).milliseconds(0)
 
   // difference between date started and the last concat (in days)
-  return chainEnd.diff(chainStart, 'days')
+  return chainEnd.diff(chainStart, 'days') + 1
 })
 
 const Chain = mongoose.model('Chain', chainSchema)
